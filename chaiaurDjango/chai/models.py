9e691a4b0bf684659b586c261fe7ea27d4260cfd
AnkitAdhikari07 <ankitadhikaricode@gmail.com>
2026-05-24 20:37:05 +0530

    Modals Created

--- chaiaurDjango/chai/models.py
+++ chaiaurDjango/chai/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class chaiVariety(models.Model):
+    CHAI_TYPE_CHOICES = [
+        ('ML', 'Masala'),
+        ('GR', 'Ginger'),
+        ('KL', 'Kiwi'),
+        ('CH', 'Chocolate'),
+        ('EL', 'Elaichi'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chais/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES)
+
+    def __str__(self):
+        return self.name
