c74616b68c315bd1bb6821c9d7c588507cfdc425
AnkitAdhikari07 <ankitadhikaricode@gmail.com>
2026-05-26 10:05:08 +0530

    Modals and URLS Completed

--- chaiaurDjango/chai/models.py
+++ chaiaurDjango/chai/models.py
@@ -13,6 +13,7 @@ class chaiVariety(models.Model):
     image = models.ImageField(upload_to='chais/')
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES)
+    description = models.TextField(default='')
 
     def __str__(self):
         return self.name
